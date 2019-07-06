f67e264784ae0cf71191da92103fd36405e40ffe
echo <echo01248@gmail.com>
2019-07-06 16:05:51 -0700

    solve buy_and_sell_stock

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -3,7 +3,16 @@ from test_framework import generic_test
 
 def buy_and_sell_stock_once(prices):
     # TODO - you fill in here.
-    return 0.0
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices[1:]:
+        profit_today = price - min_price
+        max_profit = max(profit_today, max_profit)
+
+        min_price = min(min_price, price)
+
+    return max_profit
 
 
 if __name__ == '__main__':
