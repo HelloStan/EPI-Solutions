417e392bbb5e1059017c2211037ffa00274443bc
echo <echo01248@gmail.com>
2019-07-07 16:28:08 -0700

    solve search_first_key

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -3,7 +3,40 @@ from test_framework import generic_test
 
 def search_first_of_k(A, k):
     # TODO - you fill in here.
-    return 0
+    def search(a, b):
+        m = (a + b) // 2
+
+        if a > b:
+            return -1
+
+        if A[m] < k:
+            return search(m + 1, b)
+        elif A[m] > k:
+            return search(a, m - 1)
+        else:
+            if m == 0 or A[m - 1] != k:
+                return m
+            else:
+                return search(a, m - 1)
+
+    def search_iter(a, b):
+        while a <= b:
+            m = (a + b) // 2
+            if A[m] < k:
+                a = m + 1
+            elif A[m] > k:
+                b = m - 1
+            else:
+                b = m - 1
+                if m == 0 or A[m - 1] != k:
+                    return m
+
+        return -1
+
+    # m = search(0, len(A) - 1)
+    m = search_iter(0, len(A) - 1)
+
+    return m
 
 
 if __name__ == '__main__':
