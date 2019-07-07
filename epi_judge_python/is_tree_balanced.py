acbca80223dbdadee990b8af56ddc06f819aa1b4
echo <echo01248@gmail.com>
2019-07-06 19:17:17 -0700

    solve is_tree_balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -3,7 +3,25 @@ from test_framework import generic_test
 
 def is_balanced_binary_tree(tree):
     # TODO - you fill in here.
-    return True
+    balanced = True
+
+    def explore(root, depth=0):
+        nonlocal balanced
+
+        if not root or not balanced:
+            return depth
+
+        depth_left = explore(root.left, depth+1)
+        depth_right = explore(root.right, depth+1)
+
+        if abs(depth_left - depth_right) > 1:
+            balanced = False
+
+        return max(depth_left, depth_right)
+
+    explore(tree)
+
+    return balanced
 
 
 if __name__ == '__main__':
