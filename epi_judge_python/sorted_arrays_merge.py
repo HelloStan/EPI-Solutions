b8ca25e80843faed2839c7c7b600ed8a5ae5d7e9
echo <echo01248@gmail.com>
2019-07-07 00:49:53 -0700

    solve sorted_arrays_merge

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
+import heapq
 
 
 def merge_sorted_arrays(sorted_arrays):
     # TODO - you fill in here.
-    return []
+    heap = [(array[0], i, 0) for i, array in enumerate(sorted_arrays)]
+    heapq.heapify(heap)
+
+    array = []
+    while heap:
+        val, i, j = heapq.heappop(heap)
+        
+        array.append(val)
+
+        j += 1
+        if j < len(sorted_arrays[i]):
+            heapq.heappush(heap, (sorted_arrays[i][j], i, j))
+
+    return array
 
 
 if __name__ == '__main__':
