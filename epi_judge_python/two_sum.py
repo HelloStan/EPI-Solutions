1b9b780f259f3730d03c94410b0841ad3eba8446
echo <echo01248@gmail.com>
2019-07-08 16:28:21 -0700

    solve two_sum

--- epi_judge_python/two_sum.py
+++ epi_judge_python/two_sum.py
@@ -3,7 +3,17 @@ from test_framework import generic_test
 
 def has_two_sum(A, t):
     # TODO - you fill in here.
-    return True
+    i, j = 0, len(A) - 1
+
+    while i <= j:
+        if A[i] + A[j] < t:
+            i += 1
+        elif A[i] + A[j] > t:
+            j -= 1
+        else:
+            return True
+        
+    return False
 
 
 if __name__ == '__main__':
