0c482b4046749a942591746a2185322e5dd357c6
echo <echo01248@gmail.com>
2019-07-08 16:55:09 -0700

    solve three_sum

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
 
 
+def has_two_sum(A, t):
+    # TODO - you fill in here.
+    i, j = 0, len(A) - 1
+
+    while i <= j:
+        if A[i] + A[j] < t:
+            i += 1
+        elif A[i] + A[j] > t:
+            j -= 1
+        else:
+            return True
+
+    return False
+
+
 def has_three_sum(A, t):
     # TODO - you fill in here.
-    return True
+    A.sort()
+    for v in A:
+        if has_two_sum(A, t - v):
+            return True
+    return False
 
 
 if __name__ == '__main__':
