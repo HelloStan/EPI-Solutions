82a9692a8dd46ddb5a302df1c9ec34d14594d43d
echo <echo01248@gmail.com>
2019-07-08 16:03:04 -0700

    solve number_of_score_combinations

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -3,7 +3,18 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
     # TODO - you fill in here.
-    return 0
+    combinations_for_score = [[1] + [0] * final_score for _ in range(len(individual_play_scores))]
+
+    for i in range(1, final_score + 1):
+        for j, score in enumerate(individual_play_scores):
+            if j > 0:
+                # Combinations of plays with total score i using play scores up to excluding the current score
+                combinations_for_score[j][i] += combinations_for_score[j - 1][i]
+            if i >= score:
+                # Combinations of plays for total score i-score using play scores up to including the current score
+                combinations_for_score[j][i] += combinations_for_score[j][i - score]
+
+    return combinations_for_score[-1][-1]
 
 
 if __name__ == '__main__':
