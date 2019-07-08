9c21e0bed15fd98f1b1c5ec0cc87b50705d6271c
echo <echo01248@gmail.com>
2019-07-07 17:46:07 -0700

    solve is_tree_a_bst

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -3,6 +3,18 @@ from test_framework import generic_test
 
 def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
     # TODO - you fill in here.
+    if not tree:
+        return True
+
+    if not (low_range <= tree.data <= high_range):
+        return False
+
+    if tree.left and not is_binary_tree_bst(tree.left, low_range, tree.data):
+        return False
+
+    if tree.right and not is_binary_tree_bst(tree.right, tree.data, high_range):
+        return False
+
     return True
 
 
