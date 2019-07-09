a8bc8de36ab8dc54004421f47a3cc098983a44e3
echo <echo01248@gmail.com>
2019-07-09 16:51:13 -0700

    solve parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -3,7 +3,11 @@ from test_framework import generic_test
 
 def parity(x):
     # TODO - you fill in here.
-    return 0
+    parity = 0
+    while x:
+        parity ^= x & 1
+        x >>= 1
+    return parity
 
 
 if __name__ == '__main__':
