cf35b9998a8c443087bc672a350b222fe94ec837
echo <echo01248@gmail.com>
2019-07-09 16:51:27 -0700

    solve power_x_y

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -3,7 +3,19 @@ from test_framework import generic_test
 
 def power(x, y):
     # TODO - you fill in here.
-    return 0.0
+    if y < 0:
+        x = 1.0 / x
+        y = -y
+
+    result = 1
+    while y:
+        if y & 1:
+            result *= x
+
+        y >>= 1
+        x **= 2
+
+    return result
 
 
 if __name__ == '__main__':
