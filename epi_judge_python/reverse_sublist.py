81161000f4541b286ea640f0d71d697cc24ee1d8
echo <echo01248@gmail.com>
2019-07-10 15:38:43 -0700

    solve reverse_sublist

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -1,9 +1,37 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def reverse_sublist(L, start, finish):
     # TODO - you fill in here.
-    return None
+    if L is None or start == finish:
+        return L
+
+    # Get to start position
+    parent = None
+    runner = L
+    for _ in range(1, start):
+        parent = runner
+        runner = runner.next
+
+    def reverse_sublist(parent, runner, count):
+        for i in range(count):
+            runner_next = runner.next
+            runner.next = parent
+
+            parent, runner = runner, runner_next
+
+        return parent, runner
+
+    head, ne = reverse_sublist(parent, runner, finish - start + 1)
+
+    if parent:
+        parent.next = head
+    else:
+        L = head
+    runner.next = ne
+
+    return L
 
 
 if __name__ == '__main__':
