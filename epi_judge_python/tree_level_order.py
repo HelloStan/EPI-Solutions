1c5e685a739f246f41593c84755b0535474839af
echo <echo01248@gmail.com>
2019-07-09 21:57:25 -0700

    solve tree_level_order

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -3,7 +3,26 @@ from test_framework import generic_test
 
 def binary_tree_depth_order(tree):
     # TODO - you fill in here.
-    return []
+    if tree is None:
+        return []
+
+    result = []
+
+    current_level = [tree]
+    while current_level:
+        new_level = []
+
+        result.append([])
+        for node in current_level:
+            result[-1].append(node.data)
+            if node.left:
+                new_level.append(node.left)
+            if node.right:
+                new_level.append(node.right)
+
+        current_level = new_level
+
+    return result
 
 
 if __name__ == '__main__':
