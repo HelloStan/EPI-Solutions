7e54fb85ac9a6f0a61416bbf5819ae95f3f7a62e
echo <echo01248@gmail.com>
2019-07-10 16:41:22 -0700

    solve int_square_root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -3,7 +3,19 @@ from test_framework import generic_test
 
 def square_root(k):
     # TODO - you fill in here.
-    return 0
+    a = 0
+    b = k
+
+    while a <= b:
+        x = (a + b) // 2
+        x_squared = x ** 2
+
+        if k >= x_squared:
+            a = x + 1
+        else:
+            b = x - 1
+
+    return a - 1
 
 
 if __name__ == '__main__':
