6fdcc965b140ec1660db442ca49970694683d0db
echo <echo01248@gmail.com>
2019-07-11 16:36:03 -0700

    solve k_largest_values_in_bst

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -3,7 +3,26 @@ from test_framework import generic_test, test_utils
 
 def find_k_largest_in_bst(tree, k):
     # TODO - you fill in here.
-    return None
+    k_largest = []
+
+    def traverse(tree):
+        if not tree:
+            return
+
+        if len(k_largest) >= k:
+            return
+
+        traverse(tree.right)
+
+        if len(k_largest) >= k:
+            return
+        k_largest.append(tree.data)
+
+        traverse(tree.left)
+
+    traverse(tree)
+
+    return k_largest
 
 
 if __name__ == '__main__':
