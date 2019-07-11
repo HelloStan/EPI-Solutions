3a779d1e4ca97c9c8030d54f5e05d3c80b650bd9
echo <echo01248@gmail.com>
2019-07-11 15:29:19 -0700

    solve nearest_repeated_entries

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -3,7 +3,23 @@ from test_framework import generic_test
 
 def find_nearest_repetition(paragraph):
     # TODO - you fill in here.
-    return 0
+    last_positions = {}
+    min_distances = {}
+
+    for i, word in enumerate(paragraph):
+        if word in last_positions:
+            last_position = last_positions[word]
+
+            if word in min_distances:
+                min_distances[word] = min(min_distances[word], i - last_position)
+            else:
+                min_distances[word] = i - last_position
+
+        last_positions[word] = i
+
+    min_distance = min(min_distances.values()) if len(min_distances) else -1
+
+    return min_distance
 
 
 if __name__ == '__main__':
