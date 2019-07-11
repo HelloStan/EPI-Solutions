70bd66550c8bb5e1d4754df91059e4bde28e16f5
echo <echo01248@gmail.com>
2019-07-10 17:53:21 -0700

    solve kth_largest_in_array

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -1,4 +1,5 @@
 from test_framework import generic_test
+import random
 
 
 # The numbering starts from one, i.e., if A = [3, 1, -1, 2]
@@ -6,7 +7,36 @@ from test_framework import generic_test
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k, A):
     # TODO - you fill in here.
-    return 0
+    def swap(a, b):
+        A[a], A[b] = A[b], A[a]
+
+    def partition(a, b, pivot_index):
+        pivot = A[pivot_index]
+
+        swap(pivot_index, b)
+
+        pivot_index = a
+        for i in range(a, b):
+            if A[i] > pivot:
+                swap(i, pivot_index)
+                pivot_index += 1
+
+        swap(b, pivot_index)
+
+        return pivot_index
+
+    a = 0
+    b = len(A) - 1
+    while a <= b:
+        m = random.randint(a, b)
+        pivot_index = partition(a, b, m)
+
+        if pivot_index > k - 1:
+            b = pivot_index - 1
+        elif pivot_index < k - 1:
+            a = pivot_index + 1
+        else:
+            return A[pivot_index]
 
 
 if __name__ == '__main__':
