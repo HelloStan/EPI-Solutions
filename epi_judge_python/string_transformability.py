37073b92eeeb0842a44658345fac60b51cbc6d79
echo <echo01248@gmail.com>
2019-07-11 18:46:58 -0700

    solve string_transformability

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,10 +1,31 @@
 from test_framework import generic_test
+from collections import deque
+import string
 
 
 # Uses BFS to find the least steps of transformation.
 def transform_string(D, s, t):
     # TODO - you fill in here.
-    return 0
+    queue = deque()
+
+    queue.append((0, s))
+    D.remove(s)
+
+    while queue:
+        dist, word = queue.popleft()
+
+        for i in range(len(word)):
+            for c in string.ascii_lowercase:
+                new_word = word[:i] + c + word[i+1:]
+
+                if new_word in D:
+                    D.remove(new_word)
+                    queue.append((dist + 1, new_word))
+
+                    if new_word == t:
+                        return dist + 1
+
+    return -1
 
 
 if __name__ == '__main__':
