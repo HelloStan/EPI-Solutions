6e6d0118e65bc2de859a4989b451c252fc823e8f
echo <echo01248@gmail.com>
2019-07-12 16:17:14 -0700

    solve n_queens

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -1,9 +1,43 @@
 from test_framework import generic_test
+from copy import copy
 
 
 def n_queens(n):
     # TODO - you fill in here.
-    return []
+    solution = []
+    row_placements = [-1] * n
+
+    row_spaces = set()
+    diag_a_spaces = set()
+    diag_b_spaces = set()
+
+    def solve(col, row_placements):
+        if col == n:
+            solution.append(row_placements)
+            return
+
+        for row in range(n):
+            if row in row_spaces:
+                continue
+            if (col - row) in diag_a_spaces:
+                continue
+            if (n - row - col - 1) in diag_b_spaces:
+                continue
+
+            row_spaces.add(row)
+            diag_a_spaces.add((col - row))
+            diag_b_spaces.add((n - row - col - 1))
+
+            row_placements[col] = row
+            solve(col + 1, copy(row_placements))
+
+            row_spaces.remove(row)
+            diag_a_spaces.remove((col - row))
+            diag_b_spaces.remove((n - row - col - 1))
+
+    solve(0, row_placements)
+
+    return solution
 
 
 def comp(a, b):
