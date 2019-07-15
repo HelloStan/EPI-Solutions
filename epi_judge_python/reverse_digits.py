821e3f0f70c84ab88f7b008b6a13b8794fe7eb4e
echo <echo01248@gmail.com>
2019-07-15 16:44:25 -0700

    solve reverse_digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -3,7 +3,22 @@ from test_framework import generic_test
 
 def reverse(x):
     # TODO - you fill in here.
-    return 0
+    x_rev = 0
+    negative = False
+    if x < 0:
+        x = -x
+        negative = True
+
+    while x:
+        d = x % 10
+        x_rev = 10 * x_rev + d
+
+        x //= 10
+
+    if negative:
+        x_rev = -x_rev
+
+    return x_rev
 
 
 if __name__ == '__main__':
