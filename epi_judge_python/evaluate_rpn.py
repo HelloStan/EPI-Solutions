f6862586d8cd0eb70627f690ad8a0bdf3f1ab69c
echo <echo01248@gmail.com>
2019-07-16 19:06:08 -0700

    solve evaluate_rpn

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -3,7 +3,38 @@ from test_framework import generic_test
 
 def evaluate(expression):
     # TODO - you fill in here.
-    return 0
+    operators = {
+                    '+': lambda a, b: a + b,
+                    '-': lambda a, b: a - b,
+                    '*': lambda a, b: a * b,
+                    '/': lambda a, b: a // b,
+    }
+
+    stack = []
+    for tocken in expression.split(','):
+        if tocken in operators:
+            if len(stack) < 2:
+                return False
+
+            B = stack.pop()
+            A = stack.pop()
+            C = operators[tocken](A, B)
+
+            stack.append(C)
+        else:
+            if tocken[0] == '-':
+                if not tocken[1:].isdigit():
+                    return False
+            else:
+                if not tocken[0:].isdigit():
+                    return False
+
+            stack.append(int(tocken))
+
+    if len(stack) == 1:
+        return stack[0]
+
+    return False
 
 
 if __name__ == '__main__':
