d6b1c3d9d4feca1b78e47d0a7ab874dbaf08af66
echo <echo01248@gmail.com>
2019-07-16 19:37:28 -0700

    solve is_tree_symmetric

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -3,7 +3,17 @@ from test_framework import generic_test
 
 def is_symmetric(tree):
     # TODO - you fill in here.
-    return True
+    def traverse(left, right):
+        if left is None and right is None:
+            return True
+
+        if left and right:
+            if left.data == right.data and traverse(left.left, right.right) and traverse(left.right, right.left):
+                return True
+
+        return False
+
+    return traverse(tree.left, tree.right) if tree else True
 
 
 if __name__ == '__main__':
