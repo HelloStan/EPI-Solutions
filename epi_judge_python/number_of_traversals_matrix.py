77435cd23dea34332df91dc6729d280b68a0ec00
echo <echo01248@gmail.com>
2019-07-17 16:30:48 -0700

    solve number_of_traversals_matrix

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -3,7 +3,18 @@ from test_framework import generic_test
 
 def number_of_ways(n, m):
     # TODO - you fill in here.
-    return 0
+    grid = [[0]*m for _ in range(n)]
+
+    for i in range(n):
+        grid[i][0] = 1
+    for j in range(m):
+        grid[0][j] = 1
+
+    for i in range(1, n):
+        for j in range(1, m):
+            grid[i][j] = grid[i-1][j] + grid[i][j-1]
+
+    return grid[-1][-1]
 
 
 if __name__ == '__main__':
