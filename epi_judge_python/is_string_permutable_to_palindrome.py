fbfe34bdf9418618411b329792e57d78157463c7
echo <echo01248@gmail.com>
2019-07-16 21:15:58 -0700

    solve is_string_permutable_to_palindrome

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,9 +1,20 @@
 from test_framework import generic_test
+from collections import defaultdict
 
 
 def can_form_palindrome(s):
     # TODO - you fill in here.
-    return True
+    counts = defaultdict(int)
+
+    for c in s:
+        counts[c] += 1
+
+    num_odd = 0
+    for v in counts.values():
+        if v % 2:
+            num_odd += 1
+
+    return num_odd <= 1
 
 
 if __name__ == '__main__':
