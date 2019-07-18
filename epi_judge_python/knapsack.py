b4011affd41149d5b7d8d68b603ac41c3561badb
echo <echo01248@gmail.com>
2019-07-17 17:19:03 -0700

    solve knapsack

--- epi_judge_python/knapsack.py
+++ epi_judge_python/knapsack.py
@@ -9,7 +9,17 @@ Item = collections.namedtuple('Item', ('weight', 'value'))
 
 def optimum_subject_to_capacity(items, capacity):
     # TODO - you fill in here.
-    return 0
+    v = [[0] * (capacity + 1) for _ in items + [0]]
+
+    for i in range(1, len(items) + 1):
+        for c in range(1, capacity + 1):
+            item = items[i - 1]
+            if item.weight <= c:
+                v[i][c] = max(v[i - 1][c], v[i - 1][c - item.weight] + item.value)
+            else:
+                v[i][c] = v[i - 1][c]
+
+    return max(v[-1])
 
 
 @enable_executor_hook
