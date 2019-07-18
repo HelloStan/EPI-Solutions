aee2bfa109a9119af5bc9a0b16852f5048ab669c
echo <echo01248@gmail.com>
2019-07-17 17:41:22 -0700

    solve matrix_connected_regions

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -3,6 +3,21 @@ from test_framework import generic_test
 
 def flip_color(x, y, image):
     # TODO - you fill in here.
+    def is_valid_point(x, y, color):
+        return 0 <= x < len(image) and 0 <= y < len(image[0]) and image[x][y] == color
+
+    def explore_color(x, y, color):
+        if not is_valid_point(x, y, color):
+            return
+
+        image[x][y] = not color
+
+        for dx, dy in [(0, -1), (0, +1), (-1, 0), (+1, 0)]:
+            explore_color(x + dx, y + dy, color)
+
+    color = image[x][y]
+    explore_color(x, y, color)
+
     return
 
 
