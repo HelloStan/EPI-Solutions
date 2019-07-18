24428d52dd9d495fb7dd6c92d7dc38a91ce05fc6
echo <echo01248@gmail.com>
2019-07-18 16:19:05 -0700

    solve permutations

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -3,7 +3,19 @@ from test_framework import generic_test, test_utils
 
 def permutations(A):
     # TODO - you fill in here.
-    return []
+    def gen_permutations(k):
+        if k == 0:
+            permutations.append(A.copy())
+
+        for i in range(0, k + 1):
+            A[i], A[k] = A[k], A[i]
+            gen_permutations(k - 1)
+            A[i], A[k] = A[k], A[i]
+
+    permutations = []
+    gen_permutations(len(A) - 1)
+
+    return permutations
 
 
 if __name__ == '__main__':
