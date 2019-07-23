8a97923c5de536d657d4c9c3ebeac8391d89a016
echo <echo01248@gmail.com>
2019-07-23 12:04:21 -0700

    solve even_odd_list_merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -1,9 +1,31 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def even_odd_merge(L):
     # TODO - you fill in here.
-    return None
+    even_head = ListNode(0, None)
+    odd_head = ListNode(0, None)
+
+    even_runner = even_head
+    odd_runner = odd_head
+
+    even = False
+    while L:
+        if even:
+            even_runner.next = L
+            even_runner = even_runner.next
+        else:
+            odd_runner.next = L
+            odd_runner = odd_runner.next
+
+        L = L.next
+        even = not even
+
+    odd_runner.next = even_head.next
+    even_runner.next = None
+
+    return odd_head.next
 
 
 if __name__ == '__main__':
