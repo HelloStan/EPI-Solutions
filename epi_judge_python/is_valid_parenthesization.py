10ae9435c573f7ff508c66735054b9057a5f8c50
echo <echo01248@gmail.com>
2019-07-23 12:13:57 -0700

    solve is_valid_parenthesization

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -3,7 +3,26 @@ from test_framework import generic_test
 
 def is_well_formed(s):
     # TODO - you fill in here.
-    return True
+    stack = []
+
+    for c in s:
+        if c not in "(){}[]":
+            return False
+
+        if c in "({[":
+            stack.append(c)
+        else:
+            if len(stack) == 0:
+                return False
+            if c == ')' and stack[-1] != '(':
+                return False
+            if c == '}' and stack[-1] != '{':
+                return False
+            if c == ']' and stack[-1] != '[':
+                return False
+            stack.pop()
+
+    return len(stack) == 0
 
 
 if __name__ == '__main__':
