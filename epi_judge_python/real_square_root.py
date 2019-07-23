a6f82ffc7902a8ca98681c0bec6edf9effa8d178
echo <echo01248@gmail.com>
2019-07-23 13:42:49 -0700

    solve real_square_root

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
+import math
 
 
 def square_root(x):
     # TODO - you fill in here.
-    return 0.0
+    left, right = (x, 1.0) if x < 1.0 else (1.0, x)
+
+    while not math.isclose(left, right):
+        m = (left + right) / 2
+
+        if m ** 2 > x:
+            right = m
+        else:
+            left = m
+
+    print(x, left)
+    return left
 
 
 if __name__ == '__main__':
