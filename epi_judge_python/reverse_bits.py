cab20c6de71816b32343cc02d31b442b111f6f49
echo <echo01248@gmail.com>
2019-07-22 21:15:22 -0700

    solve reverse_bits

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,30 @@
 from test_framework import generic_test
 
 
+def reverse_byte(x):
+    rev = 0
+    for i in range(8):
+        rev = (rev << 1) | (x & 1)
+        x >>= 1
+    return rev
+
+
+reverse_lookup = None
+
+
 def reverse_bits(x):
     # TODO - you fill in here.
-    return 0
+
+    global reverse_lookup
+    if reverse_lookup is None:
+        reverse_lookup = [reverse_byte(x) for x in range(256)]
+
+    rev = 0
+    for i in range(8):
+        rev = (rev << 8) | reverse_lookup[x & 0xFF]
+        x >>= 8
+
+    return rev
 
 
 if __name__ == '__main__':
