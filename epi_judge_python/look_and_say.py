bc56c6e250b6d6539695e899f8877b99d7a04f06
echo <echo01248@gmail.com>
2019-07-23 11:41:23 -0700

    solve look_and_say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -3,7 +3,25 @@ from test_framework import generic_test
 
 def look_and_say(n):
     # TODO - you fill in here.
-    return ''
+    def next_string(s):
+        say_s = []
+        i = 0
+        while i < len(s):
+            j = i
+            while j < len(s) and s[i] == s[j]:
+                j += 1
+
+            say_s.append(str(j - i))
+            say_s.append(s[i])
+            i = j
+
+        return ''.join(say_s)
+
+    s = '1'
+    for i in range(n - 1):
+        s = next_string(s)
+
+    return s
 
 
 if __name__ == '__main__':
