bc3d9a564608e31a5bd7f7c7a02fb6dd9f4e2cb3
echo <echo01248@gmail.com>
2019-07-23 12:50:20 -0700

    solve tree_with_parent_inorder

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -3,7 +3,23 @@ from test_framework import generic_test
 
 def inorder_traversal(tree):
     # TODO - you fill in here.
-    return []
+    traversal = []
+
+    last_node = None
+    while tree:
+        if last_node is tree.parent:
+            if not tree.left:
+                traversal.append(tree.data)
+            next_node = tree.left or tree.right or tree.parent
+        elif last_node is tree.left:
+            traversal.append(tree.data)
+            next_node = tree.right or tree.parent
+        else:
+            next_node = tree.parent
+
+        last_node = tree
+        tree = next_node
+    return traversal
 
 
 if __name__ == '__main__':
