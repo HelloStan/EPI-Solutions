d60ffd8e59158aeabf78baf83e6f15a1298f8397
echo <echo01248@gmail.com>
2019-07-24 16:30:58 -0700

    solve bst_from_sorted_array

--- epi_judge_python/bst_from_sorted_array.py
+++ epi_judge_python/bst_from_sorted_array.py
@@ -6,10 +6,23 @@ from test_framework.binary_tree_utils import (binary_tree_height,
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+from bst_node import BstNode
+
 
 def build_min_height_bst_from_sorted_array(A):
     # TODO - you fill in here.
-    return None
+    def build_bst(start=0, end=len(A)-1):
+        if start > end:
+            return None
+
+        m = (start + end) // 2
+
+        left = build_bst(start, m - 1)
+        right = build_bst(m + 1, end)
+
+        return BstNode(A[m], left, right)
+
+    return build_bst()
 
 
 @enable_executor_hook
