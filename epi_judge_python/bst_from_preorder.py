f99e75a30768baae157d8ee86f9e25fb13750ea5
echo <echo01248@gmail.com>
2019-07-24 16:24:26 -0700

    solve bst_from_preorder

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
+from bst_node import BstNode
 
 
 def rebuild_bst_from_preorder(preorder_sequence):
     # TODO - you fill in here.
-    return None
+    def build_bst(lower_bound=-float('inf'), upper_bound=+float('inf')):
+        nonlocal root_idx
+
+        if root_idx >= len(preorder_sequence):
+            return None
+
+        root = preorder_sequence[root_idx]
+        if not lower_bound < root < upper_bound:
+            return None
+
+        root_idx += 1
+
+        left = build_bst(lower_bound, root)
+        right = build_bst(root, upper_bound)
+
+        return BstNode(root, left, right)
+
+    root_idx = 0
+    return build_bst()
 
 
 if __name__ == '__main__':
