0e4ecc7a99e98899145af960306d30676edc86d0
echo <echo01248@gmail.com>
2019-07-25 15:14:38 -0700

    improve is_string_in_matrix with memoization

--- epi_judge_python/is_string_in_matrix.py
+++ epi_judge_python/is_string_in_matrix.py
@@ -6,23 +6,32 @@ def is_pattern_contained_in_grid(grid, S):
     def is_valid(x, y):
         return 0 <= x < len(grid) and 0 <= y < len(grid[0])
 
-    prev_valid = [[True]*len(grid[0]) for _ in range(len(grid))]
+    failed_suffixes = set()  # (x, y, offset)
 
-    for v in S:
-        current_valid = [[False]*len(grid[0]) for _ in range(len(grid))]
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] == v:
-                    for dx, dy in [(0, -1), (0, +1), (-1, 0), (+1, 0)]:
-                        nx, ny = x + dx, y + dy
-                        if is_valid(nx, ny) and prev_valid[nx][ny]:
-                            current_valid[x][y] = True
-                        if len(S) == 1:
-                            current_valid[x][y] = True
+    def is_suffix_found(x, y, offset):
+        if offset == len(S):
+            return True
 
-        prev_valid = current_valid
+        if not is_valid(x, y):
+            return False
+        if grid[x][y] != S[offset]:
+            return False
+        if (x, y, offset) in failed_suffixes:
+            return False
 
-    return max([max(row) for row in prev_valid])
+        for dx, dy in [(0, -1), (0, +1), (-1, 0), (+1, 0)]:
+            if is_suffix_found(x + dx, y + dy, offset + 1):
+                return True
+
+        failed_suffixes.add((x, y, offset))
+        return False
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if is_suffix_found(x, y, 0):
+                return True
+
+    return False
 
 
 if __name__ == '__main__':
