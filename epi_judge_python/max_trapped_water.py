fdfd0befed5a13c238e75420c337aa0d85259e5e
echo <echo01248@gmail.com>
2019-07-25 16:31:32 -0700

    solve max_trapped_water

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -3,7 +3,20 @@ from test_framework import generic_test
 
 def get_max_trapped_water(heights):
     # TODO - you fill in here.
-    return 0
+    i = 0
+    j = len(heights) - 1
+
+    max_area = 0
+    while i < j:
+        area = min(heights[i], heights[j]) * (j - i)
+        max_area = max(max_area, area)
+
+        if heights[i] < heights[j]:
+            i += 1
+        else:
+            j -= 1
+
+    return max_area
 
 
 if __name__ == '__main__':
