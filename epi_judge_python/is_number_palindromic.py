c33da7eb8a96162c91fb8721d93eac8532aaae76
echo <echo01248@gmail.com>
2019-08-07 14:13:05 -0700

    solve is_number_palindromic

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,9 +1,31 @@
 from test_framework import generic_test
 
 
+def reverse(x):
+    # TODO - you fill in here.
+    x_rev = 0
+    negative = False
+    if x < 0:
+        x = -x
+        negative = True
+
+    while x:
+        d = x % 10
+        x_rev = 10 * x_rev + d
+
+        x //= 10
+
+    if negative:
+        x_rev = -x_rev
+
+    return x_rev
+
+
 def is_palindrome_number(x):
     # TODO - you fill in here.
-    return True
+    rev_x = reverse(x)
+
+    return x >= 0 and x == rev_x
 
 
 if __name__ == '__main__':
