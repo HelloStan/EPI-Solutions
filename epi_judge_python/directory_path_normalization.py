8ed55f491b36fef81633c0d25863e5e02854f102
echo <echo01248@gmail.com>
2019-08-07 16:00:07 -0700

    solve directory_path_normalization

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -3,7 +3,28 @@ from test_framework import generic_test
 
 def shortest_equivalent_path(path):
     # TODO - you fill in here.
-    return ''
+    is_absolute = path[0] == '/'
+    path = path.split('/')
+
+    stack = []
+
+    for token in path:
+        if token == '.' or len(token) == 0:
+            continue
+        elif token == '..':
+            if len(stack) > 0 and stack[-1] != '..':
+                stack.pop()
+            else:
+                stack.append(token)
+        else:
+            stack.append(token)
+
+    path = '/'.join(stack)
+
+    if is_absolute:
+        path = '/' + path
+
+    return path
 
 
 if __name__ == '__main__':
