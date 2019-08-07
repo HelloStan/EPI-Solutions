2ed431b22b11f92fce87d8f19e4b845d564ce7f8
echo <echo01248@gmail.com>
2019-08-07 15:12:23 -0700

    solve is_list_palindromic

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -1,8 +1,40 @@
 from test_framework import generic_test
 
 
+def reverse_list(L):
+    prev_node, node = None, L
+
+    while node:
+        next_node = node.next
+        node.next = prev_node
+        prev_node = node
+        node = next_node
+
+        # prev_node, node, node.next = node, node.next, prev_node
+
+    return prev_node
+
+
 def is_linked_list_a_palindrome(L):
     # TODO - you fill in here.
+    prev_slow, slow, fast = None, L, L
+    while fast and fast.next:
+        prev_slow = slow
+        slow = slow.next
+        fast = fast.next.next
+
+    L1 = L
+    # if prev_slow:
+    #     prev_slow.next = None
+    L2 = reverse_list(slow)
+
+    while L1 and L2:
+        if L1.data != L2.data:
+            return False
+
+        L1 = L1.next
+        L2 = L2.next
+
     return True
 
 
