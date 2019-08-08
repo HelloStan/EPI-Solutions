485995a3d6cb34ecd946adfc503111d82566b285
echo <echo01248@gmail.com>
2019-08-08 16:46:59 -0700

    solve longest_nondecreasing_subsequence

--- epi_judge_python/longest_nondecreasing_subsequence.py
+++ epi_judge_python/longest_nondecreasing_subsequence.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def longest_nondecreasing_subsequence_length(A):
     # TODO - you fill in here.
-    return 0
+    max_length = [1] * len(A)
+
+    for i in range(len(A)):
+        for j in range(i):
+            if A[j] <= A[i]:
+                max_length[i] = max(max_length[i], max_length[j] + 1)
+
+    return max(max_length)
 
 
 if __name__ == '__main__':
