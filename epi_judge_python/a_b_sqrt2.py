a0c9478470a326e73e59844e730eb843d10609b2
echo <echo01248@gmail.com>
2019-08-08 15:49:48 -0700

    solve a_b_sqrt2

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
+from sortedcontainers import SortedDict
 
 
 def generate_first_k_a_b_sqrt2(k):
     # TODO - you fill in here.
-    return []
+    candidates = SortedDict()
+
+    candidates[0] = (0, 0)
+
+    result = []
+    while candidates and len(result) < k:
+        v, (a, b) = candidates.popitem(0)
+
+        result.append(v)
+
+        candidates[(a + 1) + b * 2 ** 0.5] = a + 1, b
+        candidates[a + (b + 1) * 2 ** 0.5] = a, b + 1
+
+    return result
 
 
 if __name__ == '__main__':
