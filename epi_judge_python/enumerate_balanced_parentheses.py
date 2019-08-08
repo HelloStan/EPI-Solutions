93b02271800a3254a1e046a9029abeb03b1beed4
echo <echo01248@gmail.com>
2019-08-08 16:20:08 -0700

    solve enumerate_balanced_parentheses

--- epi_judge_python/enumerate_balanced_parentheses.py
+++ epi_judge_python/enumerate_balanced_parentheses.py
@@ -3,7 +3,20 @@ from test_framework import generic_test, test_utils
 
 def generate_balanced_parentheses(num_pairs):
     # TODO - you fill in here.
-    return []
+    def generate(left_needed, right_needed, prefix=""):
+        if left_needed > 0:
+            generate(left_needed - 1, right_needed, prefix + "(")
+
+        if right_needed > left_needed:
+            generate(left_needed, right_needed - 1, prefix + ")")
+
+        if left_needed == 0 and right_needed == 0:
+            result.append(prefix)
+
+    result = []
+    generate(num_pairs, num_pairs)
+
+    return result
 
 
 if __name__ == '__main__':
