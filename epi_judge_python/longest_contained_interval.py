05db5a310d6560d43212432e54f9b56feeb778d2
echo <echo01248@gmail.com>
2019-08-07 18:19:35 -0700

    solve longest_contained_interval

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -3,7 +3,25 @@ from test_framework import generic_test
 
 def longest_contained_range(A):
     # TODO - you fill in here.
-    return 0
+    entries = set(A)
+
+    largest_set = 0
+    while entries:
+        x = entries.pop()
+
+        lower_bound = x
+        while lower_bound - 1 in entries:
+            lower_bound -= 1
+            entries.remove(lower_bound)
+
+        upper_bound = x
+        while upper_bound + 1 in entries:
+            upper_bound += 1
+            entries.remove(upper_bound)
+
+        largest_set = max(largest_set, upper_bound - lower_bound + 1)
+
+    return largest_set
 
 
 if __name__ == '__main__':
