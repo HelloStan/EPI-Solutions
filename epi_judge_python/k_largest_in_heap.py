af848e067f41aa2860cae4853b93c69b39fe2db0
echo <echo01248@gmail.com>
2019-08-07 17:34:29 -0700

    solve k_largest_in_heap

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -1,9 +1,27 @@
 from test_framework import generic_test, test_utils
+import heapq
 
 
 def k_largest_in_binary_heap(A, k):
     # TODO - you fill in here.
-    return []
+    heap = []
+    heapq.heappush(heap, (-A[0], 0))
+
+    result = []
+    for _ in range(k):
+        i = heapq.heappop(heap)[1]
+
+        result.append(A[i])
+
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < len(A):
+            heapq.heappush(heap, (-A[left], left))
+        if right < len(A):
+            heapq.heappush(heap, (-A[right], right))
+
+    return result
 
 
 if __name__ == '__main__':
