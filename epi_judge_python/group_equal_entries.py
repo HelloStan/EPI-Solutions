4d2520b6d0b03c228f1c16ad3e87cf8ce7a7fd09
echo <echo01248@gmail.com>
2019-08-08 15:26:17 -0700

    solve group_equal_entries

--- epi_judge_python/group_equal_entries.py
+++ epi_judge_python/group_equal_entries.py
@@ -8,8 +8,38 @@ from test_framework.test_utils import enable_executor_hook
 Person = collections.namedtuple('Person', ('age', 'name'))
 
 
+from collections import defaultdict
+
+
 def group_by_age(people):
     # TODO - you fill in here.
+    counts = defaultdict(int)
+
+    for person in people:
+        counts[person.age] += 1
+
+    offset = 0
+    age_to_offset = {}
+
+    for age, count in counts.items():
+        age_to_offset[age] = offset
+        offset += count
+
+    while counts:
+        from_age = next(iter(counts))
+        from_index = age_to_offset[from_age]
+
+        to_age = people[from_index].age
+        to_index = age_to_offset[to_age]
+
+        people[from_index], people[to_index] = people[to_index], people[from_index]
+
+        age_to_offset[to_age] += 1
+
+        counts[to_age] -= 1
+        if counts[to_age] == 0:
+            del counts[to_age]
+
     return
 
 
