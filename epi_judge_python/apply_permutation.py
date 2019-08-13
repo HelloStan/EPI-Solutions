06e3c0c6c757ef111a61b8deb89e731fa914241b
echo <echo01248@gmail.com>
2019-08-12 20:29:30 -0700

    solve apply_permutation

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -3,6 +3,11 @@ from test_framework import generic_test
 
 def apply_permutation(perm, A):
     # TODO - you fill in here.
+    for i in range(len(A)):
+        while perm[i] != i:
+            A[i], A[perm[i]] = A[perm[i]], A[i]
+            perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
+
     return
 
 
