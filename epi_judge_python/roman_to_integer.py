46cbf75a31d05183ad10f3a83f9cc6b898ab8f17
echo <echo01248@gmail.com>
2019-08-12 20:06:34 -0700

    solve roman_to_integer

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -3,7 +3,27 @@ from test_framework import generic_test
 
 def roman_to_integer(s):
     # TODO - you fill in here.
-    return 0
+    maping = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    sum = 0
+    prev_d = float('inf')
+    for c in s:
+        d = maping[c]
+        sum += d
+        if d > prev_d:
+            sum -= 2 * prev_d
+
+        prev_d = d
+
+    return sum
 
 
 if __name__ == '__main__':
