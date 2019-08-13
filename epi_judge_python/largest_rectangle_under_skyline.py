fed844417272692c76b5ea3c951329b41f8d4d30
echo <echo01248@gmail.com>
2019-08-12 19:59:01 -0700

    solve largest_rectangle_under_skyline

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -3,7 +3,19 @@ from test_framework import generic_test
 
 def calculate_largest_rectangle(heights):
     # TODO - you fill in here.
-    return 0
+    pillars = []
+    max_area = 0
+
+    for i, h in enumerate(heights + [0]):
+        while pillars and heights[pillars[-1]] >= h:
+            height = heights[pillars.pop()]
+            width = i - pillars[-1] - 1 if pillars else i
+            area = width * height
+            max_area = max(max_area, area)
+
+        pillars.append(i)
+
+    return max_area
 
 
 if __name__ == '__main__':
