2bfcbea77b482ea578993f65d6aa1e3db23b4f8c
echo <echo01248@gmail.com>
2019-10-15 19:15:53 -0700

    solve phone_number_mnemonic

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -3,7 +3,26 @@ from test_framework import generic_test, test_utils
 
 def phone_mnemonic(phone_number):
     # TODO - you fill in here.
-    return []
+    mapping = ('0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ')
+
+    mnemonics = []
+    mnemonic = []
+
+    def func(offset=0):
+        if offset == len(phone_number):
+            mnemonics.append(''.join(mnemonic))
+            return
+
+        digit = phone_number[offset]
+        digit = int(digit)
+        for letter in mapping[digit]:
+            mnemonic.append(letter)
+            func(offset + 1)
+            mnemonic.pop()
+
+    func()
+
+    return mnemonics
 
 
 if __name__ == '__main__':
