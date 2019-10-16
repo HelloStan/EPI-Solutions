55bbcc448aca54850cfef110435d863e93165270
echo <echo01248@gmail.com>
2019-10-15 19:04:25 -0700

    solve minimum_weight_path_in_a_triangle

--- epi_judge_python/minimum_weight_path_in_a_triangle.py
+++ epi_judge_python/minimum_weight_path_in_a_triangle.py
@@ -3,7 +3,11 @@ from test_framework import generic_test
 
 def minimum_path_weight(triangle):
     # TODO - you fill in here.
-    return 0
+    for i in range(len(triangle) - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+
+    return triangle[0][0] if len(triangle) > 0 else 0
 
 
 if __name__ == '__main__':
